a5c4af9ed04eb97a6e77117f51345d67dbc09636
Jasun Feddema and Kaila Goff <kaila@>
2018-03-12 13:27:54 -0700

    fix backend and ui logic to get cards to flip over

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import './styles.css';
 import {startGame} from './../src/memory.js';
+
 window.onload = startGame();
+// 
+// let card = document.getElementsByClassName('card');
+// let cards = [...card];
+
+// for(var i = 0; i < cards.length; i++){
+//   cards[i].addEventListener("click", displayCard);
+// }
+
+// var displayCard = function() {
+//   this.classList.toggle('open');
+//   this.classList.toggle('show');
+//   this.classList.toggle('disabled');
+// };
--- src/memory.js
+++ src/memory.js
@@ -1,55 +1,57 @@
-
-
-var displayCard = function() {
-  this.classList.toggle('open');
-  this.classList.toggle('show');
-  this.classList.toggle('disabled');
-}
-
+var openedCards = [];
 let card = document.getElementsByClassName('card');
 let cards = [...card];
-
-for(var i = 0; i < cards.length; i++){
-  cards[i].addEventListener("click", displayCard);
-};
 
 // fischer-yates shuffle
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
   while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex)
+    randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-  return array
+  return array;
 }
 
 // deck of all of the cards
-const deck = document.querySelector(".deck");
+
 export function startGame() {
+  const deck = document.querySelector(".deck");
+  let card = document.getElementsByClassName('card');
+  let cards = [...card];
   var shuffledCards = shuffle(cards);
-  for (var i = 0; i < shuffledCards.length; i++){
+  var displayCard = function() {
+    this.classList.toggle('open');
+    this.classList.toggle('show');
+    this.classList.toggle('disabled');
+  };
+  for(var i = 0; i < cards.length; i++){
+    cards[i].addEventListener("click", displayCard);
+  }
+  for (var j = 0; j < shuffledCards.length; j++){
+    deck.innerHTML = "";
     [].forEach.call(shuffledCards, function(item){
       deck.appendChild(item);
     });
+    cards[j].classList.remove("show", "open", "match", "disabled");
   }
 }
 
-function cardOpen() {
+export function cardOpen() {
   openedCards.push(this);
   var len = openedCards.length;
   if(len === 2){
-    moveCounter();
+    // moveCounter();
     if(openedCards[0].type === openedCards[1].type){
       matched();
     } else {
       unmatched();
     }
   }
-};
+}
 
 function matched(){
   openedCards[0].classList.add("match");
@@ -79,10 +81,8 @@ function disable(){
 function enable(){
   Array.prototype.filter.call(cards, function(card){
     card.classList.remove('disabled');
-    for(var i = 0; i < matchedCard.length; i++){
-      matchedCard[i].classList.add('disabled');
+    for(var i = 0; i < 2; i++){
+      [i].classList.add('disabled');
     }
   });
 }
-
-startGame
